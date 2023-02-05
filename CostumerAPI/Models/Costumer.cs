c3c75aeebf62e2de26e3f7408282bafe45238ba9
Paul Zarate <paul.zart1@gmail.com>
2023-02-05 01:26:58 -0500

    Creacion de las columnas que tendra nuestra tabla producto, con los siguientes atributos: ID del cliente (cotumer_id), nombre del cliente (costumer_name), numero de telefono del cliente (mobile_no), y un correo (email)

--- CostumerAPI/Models/Costumer.cs
+++ CostumerAPI/Models/Costumer.cs
@@ -1,6 +1,24 @@
-﻿namespace CostumerAPI.Models
+﻿using Swashbuckle.AspNetCore.SwaggerGen;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CostumerAPI.Models
 {
+    [Table("costumer", Schema = "dbo")]
     public class Costumer
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("costumer_id")]
+        public int CostumerId { get; set; }
+
+        [Column("costumer_name")]
+        public string CostumerName { get; set; }
+
+        [Column("mobile_no")]
+        public string MobileNumber { get; set; }
+
+        [Column("email")]
+        public string Email { get; set; }
     }
 }
