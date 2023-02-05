1e4ff54132e2533888fc68a50f81d2f70ea471f3
Anthony Yñape <rigo_521@hotmail.com>
2023-02-05 01:34:03 -0500

    Se Hizo la creacion de la clase Prodcutos con sus respectivos atributos, ademas que se Hizo la clase de conexion de la base de datos para la clase Productos

--- ProductAPI/Models/Product.cs
+++ ProductAPI/Models/Product.cs
@@ -1,6 +1,23 @@
-﻿namespace ProductAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductAPI.Models
 {
+    [Table("product")]
     public class Product
     {
+       [Key]
+       [Column("product_id")]
+       public int ProductId { get; set; }
+
+       [Column("product_name")]
+       public string ProductName { get; set; }
+
+       [Column("product_code")]
+       public String ProductCode { get; set; }
+
+       [Column("product_price")]
+       public decimal ProductPrice { get; set; }
+
     }
 }
---
+++ ProductAPI/ProductDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using ProductAPI.Models;
+
+namespace ProductAPI
+{
+    public class ProductDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; } 
+        public ProductDbContext(DbContextOptions <ProductDbContext>dbContextOptions) : base (dbContextOptions)
+        {
+
+            try
+            {
+                var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if(databaseCreator != null)
+                {
+                    //Creacion de la BD (Excepciones)
+                    if(!databaseCreator.CanConnect()) databaseCreator.CanConnect();
+
+                    //Creacion de las tablas en caso no existiera
+                    if(!databaseCreator.HasTables()) databaseCreator.CreateTables();
+
+
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+                    
+        }
+
+
+
+    }
+}
