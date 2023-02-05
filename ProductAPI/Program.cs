a6efc9a357ae2a7adc7e6334e76d5cd56c27edfe
Anthony Yñape <rigo_521@hotmail.com>
2023-02-05 18:32:58 -0500

    Se elaboro los parametros en la clase ProductDbContext, y en la clase Program se se digito los parametros respectivos para la conexion con la base de datos MySql

--- ProductAPI/Program.cs
+++ ProductAPI/Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using ProductAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+/*Database Context Dependency Injection */
+
+var dbHost = "localhost";
+var dbName = "dms_product";
+var dbPassword = "P@assw0rd";
+
+var connectionString = $"server ={dbHost};port=3306:database={dbName};user=root;password={dbPassword}";
+builder.Services.AddDbContext<ProductDbContext>(o => o.UseMySQL(connectionString));
+/* =========================================== */
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
