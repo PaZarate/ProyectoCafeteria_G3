c3421ffbac1bd27be400596f0b465580434c17d1
abecit0 <aljoreyu.21@gmail.com>
2023-02-05 20:32:07 -0500

    Creacion de la clases order y orderDetalle

---
+++ OrderAPI/Modelos/OrderDetail.cs
@@ -0,0 +1,23 @@
+﻿
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace OrderAPI.Modelos
+{
+    [Serializable,BsonIgnoreExtraElements]
+    
+    public class OrderDetail
+    {
+        [BsonElement("product_id"),BsonRepresentation(BsonType.Int32)]
+        
+        public int ProductId { get; set; }
+
+        [BsonElement("quantity"),BsonRepresentation(BsonType.Decimal128)]
+        public decimal Quantity { get; set; }
+
+        [BsonElement("uni_price"),BsonRepresentation(BsonType.Decimal128)]
+        public DateTime OrderedOn { get; set; }
+        
+    }
+}
