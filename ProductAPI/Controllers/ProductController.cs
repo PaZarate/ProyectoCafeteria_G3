f3dbbfe7e30a0c2d291fa58a80595827b6e4e05d
Anthony Yñape <rigo_521@hotmail.com>
2023-02-05 19:02:38 -0500

    Se Hizo la creacion de la clase controlador de Productos con los respectivos metodos de crear, visualizar, actualizar y eliminar.

--- ProductAPI/Controllers/ProductController.cs
+++ ProductAPI/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Mysqlx;
+using ProductAPI.Models;
 
 namespace ProductAPI.Controllers
 {
@@ -7,5 +9,53 @@ namespace ProductAPI.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly ProductDbContext _dbContext;
+        public ProductController (ProductDbContext productDbContext)
+        {
+            _dbContext = productDbContext;
+
+        }
+
+        [HttpGet] 
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return _dbContext.Products;
+
+        }
+
+        [HttpGet("{productId:int}")]
+
+        public async Task<ActionResult<Product>> GetById(int productId)
+        {
+            var product = await _dbContext.Products.FindAsync(productId);
+            return product;
+        }
+        [HttpPost]
+        public async Task<ActionResult> Create(Product product)
+        {
+            await _dbContext.Products.AddAsync(product);
+            await _dbContext.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut]
+
+        public async Task<ActionResult> Update(Product product)
+        {
+            _dbContext.Products.Update(product);
+            await _dbContext.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{productId:int}")]
+
+        public async Task<ActionResult> Delete(int productId)
+        {
+           var product = await _dbContext.Products.FindAsync(productId);
+            _dbContext.Products.Remove(product);
+            await _dbContext.SaveChangesAsync();
+            return Ok();
+        }
+
     }
 }
