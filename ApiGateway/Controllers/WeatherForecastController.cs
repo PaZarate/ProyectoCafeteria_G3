8d8a37b712f2b3a7e1d6c9076c7cc0eea0616cd7
Yoshio Onuki <105018988+YoshioOnuki@users.noreply.github.com>
2023-02-07 00:43:12 -0500

    Variable Logger

--- ApiGateway/Controllers/WeatherForecastController.cs
+++ ApiGateway/Controllers/WeatherForecastController.cs
@@ -10,7 +10,7 @@ namespace ApiGateway.Controllers
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
-
+        // 
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
